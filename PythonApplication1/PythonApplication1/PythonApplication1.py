0e468b6b652b8b18da17eb84fb07bd60e354c34d
Joseph <kusskuss334@gmail.com>
2023-11-08 08:25:03 +0100

    kutya2

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
 # Basic things
 a = 2
 c = 3
@@ -10,7 +12,16 @@ class Dog:
         self.Age = Age
     def printing(self):
         print(f"The Dog's type is {self.Breed} and his age is {self.Age}")
+    def ugat(self,hang):
+        print(f"The Dog's type is {self.Breed} and his age is {self.Age} and gives the sound {hang}.")
 
  
-kutya1 = Dog("kuvasz","42")
+kutya1 = Dog("kuvasz",42)
 kutya1.printing()
+kutya2 = Dog("puli",11)
+kutya2.ugat("Vau")
+print(c)
+
+
+
+
